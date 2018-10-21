24508090bf7fa0a7bf521219f6dfb63d3fccc2cd
Diego <djvidal@uc.cl>
2018-10-21 19:39:41 -0300

    update colors and styling

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-toolbar fixed class="indigo darken-1" dark>
+  <v-toolbar fixed class="mainheader" >
     <v-toolbar-title class="mr-4">
       <span
       class="home"
       @click="navigateTo({name: 'root'})">
-      Panacea
+      <img src="static/img/logo.png" height="35">
       </span>
     </v-toolbar-title>
     <!-- <v-toolbar-items>
@@ -15,17 +15,17 @@
     <v-spacer></v-spacer>
 
     <v-toolbar-items>
-      <v-btn flat dark
+      <v-btn flat
       v-if="!$store.state.isUserLoggedIn"
       @click="navigateTo({name: 'login'})">
         Log In
       </v-btn>
-      <v-btn flat dark
+      <v-btn flat
       v-if="!$store.state.isUserLoggedIn"
       @click="navigateTo({name: 'register'})">
         Sign Up
       </v-btn>
-      <v-btn flat dark
+      <v-btn flat
       v-if="$store.state.isUserLoggedIn"
       @click="logout">
         Log out
@@ -52,8 +52,12 @@ export default {
 </script>
 
 <style scoped>
+.mainheader {
+  color: blue
+}
 .home {
   cursor: pointer;
+  vertical-align: bottom;
 }
 .home:hover {
   color: rgb(59, 235, 191)
