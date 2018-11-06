0a3ced3a1cf4a93446d2bcbfedc01137fb87df65
NotSimple <eddygarios@gmail.com>
2018-11-06 14:10:07 -0300

    add script to save template to state in /certificates/template/new

--- client/src/components/CreateTemplate.vue
+++ client/src/components/CreateTemplate.vue
@@ -11,38 +11,40 @@
                   </v-toolbar>
                   <v-card-text>
                     <v-form ref="form">
-                      <div class="error" v-html="error"/>
                       <v-text-field
                         label="Issuer Name"
-                        v-model="issuerName">
+                        v-model="config.issuerName">
                       </v-text-field>
                       <div class="file-upload-form">
                         Upload Issuer Logo:
                         <input type="file" @change="previewLogo" accept="image/*">
                       </div>
-                      <div class="image-preview" v-if="logoImage.length > 0">
-                        <img class="preview" :src="logoImage">
+                      <div class="image-preview" v-if="config.issuerImage.length > 0">
+                        <img class="preview" :src="config.issuerImage">
                       </div>
                       <v-text-field
-                        label="Certificate Name"
-                        v-model="certificateName">
+                        label="Certificate Title"
+                        v-model="config.certificateTitle">
                       </v-text-field>
                       <v-text-field
                         label="Description"
-                        v-model="certificateDescription">
+                        v-model="config.certificateDescription">
                       </v-text-field>
                       <div class="file-upload-form margin-2">
                         Upload Signature Image:
                         <input type="file" @change="previewSignature" accept="image/*">
                       </div>
-                      <div class="image-preview" v-if="logoImage.length > 0">
-                        <img class="preview" :src="signatureImage">
+                      <div class="image-preview" v-if="config.issuerSignatureLines[0].image.length > 0">
+                        <img class="preview" :src="config.issuerSignatureLines[0].image">
                       </div>
                     </v-form>
                   </v-card-text>
                   <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn flat class="primary">Enter</v-btn>
+                    <v-btn
+                      flat
+                      class="primary"
+                      @click="newTemplate">Enter</v-btn>
                   </v-card-actions>
                 </v-card>
               </v-flex>
@@ -54,13 +56,30 @@
 </template>
 
 <script>
+const bcTools = require('blockcert-tools-js')
 export default {
   data: () => ({
-    logoImage: '',
-    signatureImage: '',
-    issuerName: '',
-    certificateName: '',
-    certificateDescription: ''
+    config: {
+      // certificate display
+      displayHtml: '',
+      // recipient info
+      hashEmails: false,
+      // issuer info
+      issuerUrl: 'https://www.issuer.org',
+      issuerEmail: 'contact@issuer.org',
+      issuerName: 'Universidad del Centro',
+      issuerId: 'https://raw.githubusercontent.com/PanacertProject/BlockcertExamples/master/issuers/universidad-del-centro/issuer.json',
+      revocationList: 'https://raw.githubusercontent.com/PanacertProject/BlockcertExamples/master/issuers/universidad-del-centro/revocation-list.json',
+      issuerSignatureLines: [{jobTitle: 'University Issuer', image: '', name: 'Universidad del Centro'}],
+      issuerPublicKey: 'Cfd720Ada81563D36E53eF2904F5A9E87fD0a29c',
+      issuerImage: '',
+      // certificate info
+      certificateTitle: 'Certificate of Accomplishment',
+      certificateDescription: 'Lorem ipsum dolor sit amet, mei docendi concludaturque ad, cu nec partem graece. Est aperiam consetetur cu, expetenda moderatius neglegentur ei nam, suas dolor laudem eam an.',
+      criteriaNarrative: 'Nibh iriure ei nam, modo ridens neglegentur mel eu. At his cibo mucius.',
+      badgeId: '82a4c9f2-3588-457b-80ea-da645ac1b8cc',
+      certImage: ''
+    }
   }),
   methods: {
     previewLogo: function (event) {
@@ -68,7 +87,7 @@ export default {
       if (input.files && input.files[0]) {
         var reader = new FileReader()
         reader.onload = (e) => {
-          this.logoImage = e.target.result
+          this.config.issuerImage = e.target.result
         }
         reader.readAsDataURL(input.files[0])
       }
@@ -78,10 +97,14 @@ export default {
       if (input.files && input.files[0]) {
         var reader = new FileReader()
         reader.onload = (e) => {
-          this.signatureImage = e.target.result
+          this.config.issuerSignatureLines[0].image = e.target.result
         }
         reader.readAsDataURL(input.files[0])
       }
+    },
+    newTemplate: function () {
+      var template = bcTools.generateTemplate(this.config)
+      this.$store.dispatch('setCertificateTemplate', template)
     }
   }
 }
@@ -89,9 +112,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.error {
-  color: red
-}
 .bg {
   background-image: url('../../static/img/puppies.jpg');
   background-size: cover;
