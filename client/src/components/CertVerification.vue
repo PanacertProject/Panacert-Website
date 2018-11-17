e704c5e37bfa71d2b67c979b99d0bc3f68e8008e
NotSimple <eddygarios@gmail.com>
2018-11-17 12:22:45 -0300

    remove unnecessary images

--- client/src/components/CertVerification.vue
+++ client/src/components/CertVerification.vue
@@ -1,17 +1,33 @@
 <template>
-  <v-app class="bg">
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <h2 class="verify-header">Verify Your Certificate</h2>
+  <v-content>
+    <v-responsive
+      class="bg"
+      :aspect-ratio="3/2"
+      height="1000">
+      <v-container
+        fluid
+        fill-height>
+        <v-layout
+          align-center
+          justify-center>
+          <v-flex
+            xs12
+            sm8
+            md4>
+            <h2
+              class="verify-header">
+              Verify Your Certificate
+            </h2>
             <v-spacer></v-spacer>
-            <blockcerts-verifier disable-auto-verify allow-social-share display-mode="full"></blockcerts-verifier>
+            <blockcerts-verifier
+              disable-auto-verify
+              display-mode="full">
+            </blockcerts-verifier>
           </v-flex>
         </v-layout>
       </v-container>
-    </v-content>
-  </v-app>
+    </v-responsive>
+  </v-content>
 </template>
 
 <script>
@@ -22,7 +38,7 @@ require('@blockcerts/blockcerts-verifier/dist/main.js')
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .bg {
-  background-image: url('../../static/img/santiagopic.jpg');
+  background-image: url('../../static/img/parapag.png');
   background-size: cover;
   position: absolute;
   width: 100%;
